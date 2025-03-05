1f8603a5066ed05705855ade619a2a41fbc807bd
msakhawatali <166359266+msakhawatali@users.noreply.github.com>
2025-03-05 18:38:26 +0500

    github data add

--- index.js
+++ index.js
@@ -5,6 +5,28 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const gitHubData = {
+    "login": "mojombo",
+    "id": 1,
+    "node_id": "MDQ6VXNlcjE=",
+    "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/mojombo",
+    "html_url": "https://github.com/mojombo",
+    "followers_url": "https://api.github.com/users/mojombo/followers",
+    "following_url": "https://api.github.com/users/mojombo/following{/other_user}",
+    "gists_url": "https://api.github.com/users/mojombo/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/mojombo/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/mojombo/subscriptions",
+    "organizations_url": "https://api.github.com/users/mojombo/orgs",
+    "repos_url": "https://api.github.com/users/mojombo/repos",
+    "events_url": "https://api.github.com/users/mojombo/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/mojombo/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false
+  }
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -15,6 +37,10 @@ app.get('/goolge', (req, res) => {
 
 app.get("/Login", (req, res) => {
     res.send("<h1>sakhawat ali</h1>")
+})
+
+app.get('/github', (req, res) => {
+    res.json(gitHubData)
 })
 
 app.listen(process.env.port, () => {
